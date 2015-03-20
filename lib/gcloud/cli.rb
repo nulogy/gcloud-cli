51df9dc73966f23ed0761edad8b91d8a9f40f2f7
Arturo Pie <arturop@nulogy.com>
2015-03-20 12:39:42 -0400

    adds create_gorgon_cluster and use API key to send request

--- lib/gcloud/cli.rb
+++ lib/gcloud/cli.rb
@@ -1,7 +1,19 @@
 require "gcloud/cli/version"
 
+require "rest_client"
+
 module Gcloud
   module Cli
-    # Your code goes here...
+    def self.create_gorgon_cluster
+      url = 'http://localhost:3000/clusters'
+
+      # MY_NOTE: get API Key from a file
+      api_key = 'bcba4ac2fc35257d75adc6c1140dac019c0c34e0f423f78c0a50'
+
+      result = RestClient.post(url, api_key: api_key)
+
+      puts "############ result ##########"
+      p result
+    end
   end
 end
